0274e1b98e1bd27f5b11fc093e83bfcae68f212f
rachael444 <nnakwuchiamaka@gmail.c0m>
2025-05-20 10:38:09 +0000

    Jscript navelement

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,13 @@
+// Declare variable to store navbar element
+const navelement = document.getElementById('navbar');
 // detect scroll event on the document
-document.addEventListener('scroll',function(){
+document.addEventListener('scroll', function () {
     // console.log(window.scrollY);
-    if(window.scrollY > 70) {
-    // change navbar bg to blue
-    document.getElementById('navbar').style.backgroundColor='#0779E4';
-    document
+    if (window.scrollY > 70) {
+        // change navbar bg to blue
+        navelement.style.backgroundColor = '#0779E4';
     } else {
         // change navbar bg to ytransparent
-        document.getElementById('navbar').style.backgroundColor='transparent';
+        navelement.style.backgroundColor = 'transparent';
     }
 });
